a1f6c968bbc1ce562925008fb2b577e72cea9a23
camilosanchezdev <camilosanchezdev@gmail.com>
2020-05-20 00:09:00 -0300

    The program is published
    Simple application with C# and Winforms (.NET Framework)

---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Check_Braces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List list1;
+            list1 = new List();
+            string checkBraces = textBox1.Text;
+            for (int i = 0; i < checkBraces.Length; i++)
+            {
+                if (checkBraces.ElementAt(i) == '(' || checkBraces.ElementAt(i) == '[' || checkBraces.ElementAt(i) == '{')
+                {
+                    list1.Insert(checkBraces.ElementAt(i));
+                }
+                else
+                {
+                    if (checkBraces.ElementAt(i) == ')')
+                    {
+                        if (list1.Extract() != '(')
+                        {
+                            MessageBox.Show("Incorrect!");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if (checkBraces.ElementAt(i) == ']')
+                        {
+                            if (list1.Extract() != '[')
+                            {
+                                MessageBox.Show("Incorrect!");
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            if (checkBraces.ElementAt(i) == '}')
+                            {
+                                if (list1.Extract() != '{')
+                                {
+                                    MessageBox.Show("Incorrect!");
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (list1.Empty())
+            {
+                MessageBox.Show("Correct!");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect!");
+            }
+            
+        }
+    }
+}
---
+++ List.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Braces
+{
+    class List
+    {
+        class Node
+        {
+            public char info;
+            public Node next;
+        }
+        private Node root;
+        public List()
+        {
+            root = null;
+        }
+        public void Insert(char x)
+        {
+            Node newElement;
+            newElement = new Node();
+            newElement.info = x;
+            if (root == null)
+            {
+                newElement.next = null;
+                root = newElement;
+            }
+            else
+            {
+                newElement.next = root;
+                root = newElement;
+            }
+        }
+        public char Extract()
+        {
+            if (root != null)
+            {
+                char aux = root.info;
+                root = root.next;
+                return aux;
+            }
+            else
+            {
+                return char.MaxValue;
+            }
+        }
+        public bool Empty()
+        {
+            if (root  == null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
